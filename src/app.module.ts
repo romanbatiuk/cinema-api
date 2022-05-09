002baff9795a42bfe6ffe5b253ec44424d533600
romanbatiuk <roman.batiuk87@gmail.com>
2022-05-09 22:47:14 +0300

    connected to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose-next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-	imports: [],
+	imports: [
+		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
